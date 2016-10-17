1f2c18a4a809db3ef378ccf7c127cf38331b7fd5
montsjura <montsjura@gmail.com>
2016-10-17 19:59:57 +0200

    Solve Digits Multiplication in Crystalite Farm.

---
+++ EmpireOfCode/CrystaliteFarm/DigitsMultiplication.py
@@ -0,0 +1,9 @@
+golf = lambda n: eval("*".join(i for i in str(n) if i != '0'))
+
+# if __name__ == '__main__':
+#    # These "asserts" using only for self-checking and not necessary for auto-testing
+#    assert golf(123405) == 120
+#    assert golf(999) == 729
+#    assert golf(1000) == 1
+#    assert golf(1111) == 1
+#    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
